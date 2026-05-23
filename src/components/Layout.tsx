a1b31cc1acdc98c5e8d4fc9a2a0d737d94b0dc8f
David Brablc <129985521+darebakdavca@users.noreply.github.com>
2026-05-23 15:41:28 +0200

    feat: add icon image and update Layout component to include it in the header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,10 @@ export function Layout() {
     return (
         <div className="mx-auto max-w-5xl p-5 grid gap-4">
             <header className="bg-secondary rounded-full p-5 flex justify-between items-center">
-                <h1 className="text-3xl font-bold">Country eplorer</h1>
+                <div className="flex gap-2 items-center">
+                    <img src="/src/assets/icon.png" className="w-12" />
+                    <h1 className="text-3xl font-bold">Country eplorer</h1>
+                </div>
                 <ThemeSwitcher />
             </header>
             <main>
